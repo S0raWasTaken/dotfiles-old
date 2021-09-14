2f8096ff217b4d18fb6bd2879d46eb5479280dfa
S0raWasTaken <anosensedroid@gmail.com>
2021-09-14 11:05:52 -0500

    Add bash script to copy all files auto

---
+++ cpfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp $HOME/.config/i3/config ./i3gaps.config
+cp $HOME/.config/polybar/config ./polybar.config
+cp -r $HOME/.config/polybar/scripts ./polybar.scripts
+cp $HOME/.aliasrc ./aliases.aliasrc
+cp $HOME/.config/picom/picom.conf .
+
